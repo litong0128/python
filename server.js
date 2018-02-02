1f76aafbcafae4aa5b28ebeb2b1468b2cee3eff6
litong0128 <litong0128@126.com>
2018-02-02 11:20:16 +0800

    添加更新
    添加用户注册更新功能

--- server.js
+++ server.js
@@ -46,8 +46,8 @@ app.post('/get_user', function (req, res) {
   var connection = ndb.getConnect(db_host,db_user,db_pass,db_db);
   ndb.connectDB(connection);
   // 将注册表与连接表进行关联，把注册和未注册都显示出来
-  // var  sql = 'select distinct id,name,b.mac_addr,DATE_FORMAT(sign_time,"%Y-%m-%d %H:%i:%s") as sign_time from user_info a right join (select distinct mac_addr from user_connect_info where substring(con_time,1,10) = current_date()) b on a.mac_addr = b.mac_addr order by id,name';
-  var  sql = 'select distinct id,name,mac_addr,DATE_FORMAT(sign_time,"%Y-%m-%d %H:%i:%s") as sign_time from user_info ';
+ var  sql = 'select distinct id,name,b.mac_addr,DATE_FORMAT(sign_time,"%Y-%m-%d %H:%i:%s") as sign_time from user_info a right join (select distinct mac_addr from user_connect_info where substring(con_time,1,10) = current_date() or 1=1 )  b on a.mac_addr = b.mac_addr order by id,name';
+  // var  sql = 'select distinct id,name,mac_addr,DATE_FORMAT(sign_time,"%Y-%m-%d %H:%i:%s") as sign_time from user_info ';
   connection.query(sql,function (err, result) {
     if(err){
       console.log('[SELECT ERROR] - ',err.message);
@@ -70,6 +70,40 @@ app.post('/save_user', urlencodedParser,function (req, res) {
   console.log('name:'+name+'\n'+'mac_addr:'+mac_addr);
   var  addSql = 'INSERT INTO user_info (name,mac_addr) VALUES (?,?)';
   var  addSqlParams = [name,mac_addr];
+  //增
+  connection.query(addSql,addSqlParams,function (err, result) {
+          if(err){
+          console.log('[INSERT ERROR] - ',err.message);
+          return;
+          }        
+  
+        console.log('--------------------------INSERT----------------------------');
+        //console.log('INSERT ID:',result.insertId);        
+        console.log('INSERT ID:',result); 
+  });
+  ndb.close(connection);
+})
+
+
+// 保存变更添加用户信息
+app.post('/update_user', urlencodedParser,function (req, res) {
+  console.log('--------------------------update user----------------------------'); 
+  var connection = ndb.getConnect(db_host,db_user,db_pass,db_db);
+  ndb.connectDB(connection);
+  console.log(req.body);
+  var  id = req.body.id;
+  var  name = req.body.name;
+  var  mac_addr = req.body.mac_addr;
+  console.log('id:'+id+'\n'+'name:'+name+'\n'+'mac_addr:'+mac_addr);
+  // 如果用户为更新没有id则直接插入，如果有id根据id进行更新
+  if (id == '' || id == null) {
+    var  addSql = 'INSERT INTO user_info (name,mac_addr) VALUES (?,?)';
+    var  addSqlParams = [name,mac_addr];
+  }else {
+    var  addSql = 'update user_info set name = ? ,mac_addr = ? where id = ?';
+    var  addSqlParams = [name,mac_addr,id];
+  }
+  
   //增
   connection.query(addSql,addSqlParams,function (err, result) {
           if(err){
